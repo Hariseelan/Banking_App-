d19cac9a17bffec22eb6c3cd802392b4326b1561
Sathiyaseelan Hariharan <UT010002@unicomtic.com>
2025-05-05 13:57:53 +0530

    removed a unnecessary file

--- Banking_app.py
+++ Banking_app.py
@@ -1,92 +1,63 @@
-#_____________________________________________________________BANKING APP MODEL________________________________________________________________
+account = {}
+users = {}
+account_file = "account.txt"
+Login_file = "login.txt"
 
-#---------------ACCOUNT CREATION---------------
+#---------------Load Account Details---------------
+def load_account():
+    try :
+        with open(account_file,"r") as file :
+            for line in file :
+                parts = line.strip()
+                acc_no,name,balance = parts[0],parts[1],float(parts[2])
+                transaction = parts[3:] if len(parts) > 3 else : account[acc_no]={"name" : transaction}
+    except FileNotFoundError:
+        pass #file does not exist yet
 
-# LOGIN SYSTEM WITH MAX RETRY (3_TIME)
-# USERNAME AND PASSWORD
+#---------------Save Account Detail---------------
+def save_account():
+    with open(account_file,"w") as file:
+        for acc_no,data,in account.items():
+            line = (f"{acc_no}|{data['name']}|{data['balance']}|"+"|",join(data["transation"]))+"\n"
+            file.write(line)
 
-correct_username = "user123"
-correct_password = "pass123"
-max_attempts = 3
-attempts = 0
-
-while attempts < max_attempts:
-    username = input("Enter username: ")
-    password = input("Enter password: ")
-
-    if username == correct_username and password == correct_password:
-        print("""\t\t           =======================\n \t\t              Login successful!\n\t\t           =======================
-        *******************                      **************************  
-        *******************Welcome to Our service**************************
-        *******************                      **************************\n""")
-        break
-    else:
-        attempts += 1
-        remaining = max_attempts - attempts
-        print(f" Invalid credentials. {remaining} attempt(s) left.\n")
-
-if attempts == max_attempts:
-    print(""" Too many failed attempts. Exiting program.
-    ---------------------PLEASE------------------------
-    ----------------CONTACT OUR BANK-------------------
-    --------------------THANKYOU----------------------- """)
-    exit()
-
-
-# MENU LOOP + THROUGH ACCOUNT BALANCES
+#---------------Load Login Details---------------
+def load_login():
+    try:
+        with open(Login_file,"r") as file:
+            for line in file:
+                username,password,role,acc_no = line.strip()
+                users[username] = {"password":password,"role":role,"acc_no":acc_no}
+    except FileNotFoundError:
+        pass
 
 
-account_balances = [0]            # EXAMPLE ACCOUNT BALANCE
-selected_account = 0              # WE'RE WORKING WITH ACCOUNT DEFAULT
+#---------------Save Login Details---------------
+def save_login():
+    with open(login.file,"w") as file:
+        for username,data in user.items():
+            line = (f"{username}|{data['password']}|{data['role']}|{data['acc_no']}\n")
+            file.write(line)
 
-'''def account_balances():
-    pass
+#---------------Register New Customer---------------
+def register_customer():
+    username = input('Enter the Username : ')
+    if username in users:
+        print("Username already exists.")
+        return
+    password = input('Enter Your Password : ')
+    name = input('Enter Your name : ')
+    balance = float(input('Enter initial balance : '))
+    acc_no = str(len(account)+1001)
 
-def withdraw():
-    pass
+    users[username] = {"password" : password,"role" : "customer","acc_no" : acc_no}
+    account[acc_no] = {"name" : name,"balance":balance,"transaction":[f"Initial deposit:{balance}"]}
+    print(f"Account created! Your account number is {acc_no}")
 
-def deposit():
-    pass
-
-def show_details():
-    pass'''
-
-
-while True:
-    print("1.Check Balance")
-    print("2.Withdraw Money")
-    print("3.Deposit Money")
-    print("4.View All Account Balance") 
-    print("5.Exit")
-
-    choice = input("Enter your choice (1-5): ")
-
-    if choice == "1":
-        print(f"Account {selected_account} Balance: ${account_balances[selected_account]}")
-
-    elif choice == "2":
-        amount = float(input("Enter amount to withdraw: $"))
-        if amount <= account_balances[selected_account]:
-            account_balances[selected_account] -= amount
-            print(f"Withdrawal successful. New balance: ${account_balances[selected_account]}")
-        else:
-            print("Insufficient funds!")
-
-    elif choice == "3":
-        deposit = float(input("Enter amount to deposit: $"))
-        account_balances[selected_account] += deposit
-        print(f"Deposit successful. New balance: ${account_balances[selected_account]}")
-
-    elif choice == "4":
-        print("\n All Account Balances:")
-        for i, balance in enumerate(account_balances):
-            print(f"Account {i + 1}: ${balance}")
-        print()
-
-    elif choice == "5":
-        print("Thank you for using our bank. Goodbye!")
-        break
-
-    else:
-        print("'SORRY' Invalid choice. Please select a number between 1 and 5.\n")
-
+#---------------Admin Menu---------------
+def admin_menu():
+    while True:
+        print("\n=== Admin Menu ===")
+        print("1.View All Account Detail")
+        print("2.Exit")
+        
